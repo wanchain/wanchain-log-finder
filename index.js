08c639b64c8873437b318d9a6c2c7af5c9549cc5
Anson5555555Lee <frank5555555li@gmail.com>
2018-07-11 11:47:27 +0800

    modify

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const chalk  = require('chalk')
 const log = console.log
 const app = express()
 const SERVER_PORT = process.env.SERVER_PORT || 3000
-// const db = require('./db')
+const db = require('./db')
 
 app.set('port', SERVER_PORT)
 app.set('views', path.join(__dirname, 'views'))
@@ -29,21 +29,24 @@ function startServer() {
 	app.listen(app.get('port'), () => {
 				log(chalk.yellow(`==> 🚧 app server listening on ${SERVER_PORT}`))
 			})
-	// db.connection
-	// 	.sync()
-	// 	.then(() => {
-	// 		app.listen(app.get('port'), () => {
-	// 			log(chalk.yellow(`==> 🚧 app server listening on ${SERVER_PORT}`))
-	// 		})
-	// 	}) 
-	// 	.catch(err => {
-	// 		console.log('db connection error: ', err)
-	// 	})
+}
+
+function startServerDirect() {
+	db.connection
+		.sync()
+		.then(() => {
+			app.listen(app.get('port'), () => {
+				log(chalk.yellow(`==> 🚧 app server listening on ${SERVER_PORT}`))
+			})
+		}) 
+		.catch(err => {
+			console.log('db connection error: ', err)
+		})
 }
 
 if (require.main === module) {
   // application run directly; start app server
-  startServer();
+  startServerDirect();
 } else {
   // application imported as a module via "require": export function to create server
   module.exports = startServer;
